9fa5a2257a45581d24f80bcade56f4a82a3ae943
cai cai <caicai@caideiMac.local>
2017-03-28 15:13:47 +0800

    1.2

--- XLPopGesture/UINavigationController+XLPopGesture.m
+++ XLPopGesture/UINavigationController+XLPopGesture.m
@@ -23,58 +23,6 @@
 #import "UINavigationController+XLPopGesture.h"
 #import <objc/runtime.h>
 
-@interface UIImageView (XLScreenshot)
-
-+ (UIImageView *)xl_screenshot;
-
-@end
-
-@implementation UIImageView (XLScreenshot)
-
-+ (UIImageView *)xl_screenshot
-{
-    CGSize imageSize = CGSizeZero;
-    
-    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-    if (UIInterfaceOrientationIsPortrait(orientation)) {
-        imageSize = [UIScreen mainScreen].bounds.size;
-    } else {
-        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
-    }
-    
-    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
-        CGContextSaveGState(context);
-        CGContextTranslateCTM(context, window.center.x, window.center.y);
-        CGContextConcatCTM(context, window.transform);
-        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
-        if (orientation == UIInterfaceOrientationLandscapeLeft) {
-            CGContextRotateCTM(context, M_PI_2);
-            CGContextTranslateCTM(context, 0, -imageSize.width);
-        } else if (orientation == UIInterfaceOrientationLandscapeRight) {
-            CGContextRotateCTM(context, -M_PI_2);
-            CGContextTranslateCTM(context, -imageSize.height, 0);
-        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
-            CGContextRotateCTM(context, M_PI);
-            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
-        }
-        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
-            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
-        } else {
-            [window.layer renderInContext:context];
-        }
-        CGContextRestoreGState(context);
-    }
-    
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return [[UIImageView alloc] initWithImage:image];
-}
-
-@end
-
 @interface XLPopGestureDelegate : NSObject <UIGestureRecognizerDelegate>
 @property (nonatomic, weak) UINavigationController *navigationController;
 @end
@@ -253,8 +201,7 @@ typedef void (^XLViewControllerWillAppearInjectBlock)(UIViewController *viewCont
     [self xl_viewWillDisappear:animated];
     
     if ([self isMovingFromParentViewController]) {
-//        self.xl_snapshot = [self.navigationController.view snapshotViewAfterScreenUpdates:NO];
-        self.xl_snapshot = [UIImageView xl_screenshot];
+        self.xl_snapshot = [[UIApplication sharedApplication].keyWindow snapshotViewAfterScreenUpdates:NO];
     }
 }
 
@@ -296,17 +243,9 @@ typedef void (^XLViewControllerWillAppearInjectBlock)(UIViewController *viewCont
     
     UIViewController *lastController = [self.viewControllers lastObject];
     if (lastController.tabBarController) {
-//        lastController.xl_snapshot = [lastController.tabBarController.view snapshotViewAfterScreenUpdates:NO];
-        lastController.xl_snapshot = [UIImageView xl_screenshot];
         [viewController setHidesBottomBarWhenPushed:YES];
-    } else {
-        lastController.xl_snapshot = [UIImageView xl_screenshot];
-//        if (lastController.navigationController) {
-//            lastController.xl_snapshot = [lastController.navigationController.view snapshotViewAfterScreenUpdates:NO];
-//        } else {
-//            lastController.xl_snapshot = [lastController.view snapshotViewAfterScreenUpdates:NO];
-//        }
     }
+    lastController.xl_snapshot = [[UIApplication sharedApplication].keyWindow snapshotViewAfterScreenUpdates:NO];
     
     if (![self.interactivePopGestureRecognizer.view.gestureRecognizers containsObject:self.xl_popRecoginzer]) {
         
